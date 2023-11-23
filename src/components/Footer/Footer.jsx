37aeefcac674898d4ee48ef15ce491da11e3b98f
ellin ye <lingye0926@gmail.com>
2023-11-23 17:26:10 +0900

    navbar and footer finished

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
+import "./Footer.scss"
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className='footer'>
+      <div className="top">
+        <div className="item">
+          <h1>Categories</h1>
+          <span>String Instruments</span>
+          <span>Wind Instruments</span>
+          <span>Others</span>
+        </div>
+        <div className="item">
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Cookies</span>
+        </div>
+        <div className="item">
+          <h1>About</h1>
+          <sapn>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Sed maxime labore, doloribus hic praesentium asperiores vero 
+          aut et in accusantium aliquid ipsa possimus tempore suscipit 
+          adipisci alias, ut voluptate recusandae.
+          </sapn>
+        </div>
+        <div className="item">
+          <h1>Contact</h1>
+          <span>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Praesentium nostrum, aliquam obcaecati maiores vero corrupti facilis, 
+            laboriosam reprehenderit ducimus voluptatum quisquam possimus maxime 
+            blanditiis error ad sit quas, vel sed.
+          </span>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="left">
+          <span className='logo'>Musikstore</span>
+          <span className="copyright">
+            Copyright © 2023.All Rights Reserved
+          </span>
+        </div>
+        <div className="right">
+          <img src="" alt="./img/payment.png" />
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,58 @@
 import React from 'react'
+import { KeyboardArrowDown, Search,PersonOutlineOutlined,FavoriteBorderOutlined, ShoppingCartOutlined} from '@mui/icons-material';
+import {Link} from "react-router-dom"
+import "./Navbar.scss"
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='navbar'>
+      <div className="wrapper">
+        <div className="left">
+          <div className="item">
+            <img src="" alt="" />
+            <KeyboardArrowDown/>
+          </div>
+          <div className="item">
+            <span>USD</span>
+            <KeyboardArrowDown/>
+          </div>
+          <div className="items">
+            <Link className='link' to="/products/1">String Instruments</Link>
+          </div>
+          <div className="item">
+            <Link className='link' to= "/products/2">Wind Instruments</Link>
+          </div>
+          <div className="item">
+            <Link className='link' to="/products/3">Other Instruments</Link>
+          </div>
+        </div>
+        <div className="center">
+          <Link className='link' to="/">MUSIKSTORE</Link>
+        </div>
+        <div className="right">
+          <div className="items">
+            <Link className='link' to="/">Homepage</Link>
+          </div>
+          <div className="items">
+            <Link className='link' to="/">About</Link>
+          </div>
+          <div className="items">
+            <Link className='link' to="/">Contact</Link>
+          </div>
+          <div className="items">
+            <Link className='link' to="/">Stores</Link>
+          </div>
+          <div className="icons">
+            <Search/>
+            <PersonOutlineOutlined/>
+            <FavoriteBorderOutlined/>
+            <div className="cartIcon">
+              <ShoppingCartOutlined/>
+              <span>0</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
